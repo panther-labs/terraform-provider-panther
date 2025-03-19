4ebb524be05874ab04ce2c99b08a7435a268dd91
Markos Fountoulakis <markos.fountoulakis@ext.runpanther.io>
2025-03-19 15:49:25 +0200

    generate docs

--- examples/full-examples/http-log-source/http-log-source.tf
+++ examples/full-examples/http-log-source/http-log-source.tf
@@ -1,11 +1,11 @@
 resource "panther_httpsource" "example_http_source" {
-  integration_label       = var.integration_label
-  log_stream_type         = var.log_stream_type
-  log_types               = var.log_types
-  auth_method             = var.auth_method
-  auth_header_key         = var.auth_header_key
-  auth_secret_value       = var.auth_secret_value
- log_stream_type_options = {
-   json_array_envelope_field = var.json_array_envelope_field
- }
+  integration_label = var.integration_label
+  log_stream_type   = var.log_stream_type
+  log_types         = var.log_types
+  auth_method       = var.auth_method
+  auth_header_key   = var.auth_header_key
+  auth_secret_value = var.auth_secret_value
+  log_stream_type_options = {
+    json_array_envelope_field = var.json_array_envelope_field
+  }
 }
